ea453e1d3cb905871b89311fc594ba2c6dab8f4b
jaivik1920 <pateljaivik2025@gmail.com>
2021-11-07 20:01:45 +0530

    commit

--- src/component/Playground.js
+++ src/component/Playground.js
@@ -81,7 +81,7 @@ function Playground() {
             "language": "java", 
             "input": inputdata
         })
-        axios.post("/enforceCode", codedata).then(
+        axios.post("https://codexweb.netlify.app/.netlify/functions/enforceCode", codedata).then(
             (success) => {
                 console.log(success.data.output);
                 document.getElementById("output").innerHTML = success.data.output;
